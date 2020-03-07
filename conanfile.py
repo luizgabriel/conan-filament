b6b6bcfaa945f9a161065f71c8a108f8705c30d1
Luiz Gabriel <luizgabriel.info@gmail.com>
2020-03-07 18:12:13 -0300

    Updated to 1.4.5

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,55 @@
 from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
 
 
 class FilamentConan(ConanFile):
     name = "filament"
-    version = "dev-master"
+    version = "1.4.5"
     license = "Apache License 2.0"
     homepage = "https://github.com/google/filament"
     url = "https://github.com/luizgabriel/conan-filament"
-    description = "Filament is a real-time physically based rendering engine for Android, iOS, Windows, Linux, macOS and WASM/WebGL"
+    description = "Filament is a real-time physically based rendering engine for Android, iOS, Windows, Linux, " \
+                  "macOS and WASM/WebGL "
     topics = ("graphics", "3d", "filament", "google")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "skip_samples": [True, False], "enable_java": [True, False]}
-    default_options = {"shared": False, "skip_samples": True, "enable_java": False}
+    settings = ("os", "compiler", "build_type", "arch")
+    options = {
+        "shared": [True, False],
+        "enable_java": [True, False],
+
+        "with_filamat": [True, False],
+        "with_filameshio": [True, False],
+        "with_gltfio": [True, False],
+        "with_gltfio_core": [True, False],
+        "with_image": [True, False],
+        "with_matdbg": [True, False],
+        "with_meshoptimizer": [True, False],
+        "with_rays": [True, False],
+        "with_shaders": [True, False],
+
+        "with_sdl": [True, False],
+        "with_imgui": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "enable_java": False,
+
+        "with_filamat": False,
+        "with_filameshio": False,
+        "with_gltfio": False,
+        "with_gltfio_core": False,
+        "with_image": False,
+        "with_matdbg": False,
+        "with_meshoptimizer": False,
+        "with_rays": False,
+        "with_shaders": False,
+
+        "with_sdl": False,
+        "with_imgui": False
+    }
     generators = "cmake"
 
     def source(self):
         git = tools.Git(folder="filament")
-        git.clone("https://github.com/google/filament.git", "master")
+        git.clone("https://github.com/google/filament.git", "v" + self.version)
 
         tools.replace_in_file("filament/CMakeLists.txt", "project(TNT)",
                               '''project(TNT)
@@ -26,8 +58,8 @@ conan_basic_setup()''')
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["FILAMENT_ENABLE_JAVA"] = "ON" if self.options["enable_java"] else "OFF"
-        cmake.definitions["FILAMENT_SKIP_SAMPLES"] = "ON" if self.options["skip_samples"] else "OFF"
+        cmake.definitions["ENABLE_JAVA"] = "ON" if self.options["enable_java"] else "OFF"
+        cmake.definitions["BUILD_TESTING"] = "OFF"
         cmake.configure(source_dir="filament")
 
         return cmake
@@ -54,16 +86,17 @@ conan_basic_setup()''')
             "utils",
             "geometry",
             "smol-v",
-            "ibl",
-
-            # Extra lib tools
-            "filamat",
-            "filameshio",
-            "gltfio",
-            "gltfio_core",
-            "image",
-            "matdbg",
-            "meshoptimizer",
-            "rays",
-            "shaders",
+            "ibl"
         ]
+
+        # Extra lib tools
+        extra_libs = ["filamat", "filameshio", "gltfio", "gltfio_core", "image", "matdbg", "meshoptimizer",
+                      "rays", "shaders"]
+        for extra_lib in extra_libs:
+            if self.options["with_" + extra_lib]:
+                self.cpp_info.libs.append(extra_lib)
+
+        # External integrated libs
+        if self.options["with_sdl"]:
+            self.cpp_info.libs.append("sdl2")
+            self.cpp_info.libs.append("sdl2main")
