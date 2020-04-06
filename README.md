41d98b26a39718ee915fbb43cb3378a52adf643b
Luiz Gabriel <luizgabriel.info@gmail.com>
2020-04-06 19:54:41 -0300

    Updated to 1.5.2

--- README.md
+++ README.md
@@ -3,16 +3,10 @@ The Google's Filament Rendering Engine as a conan package.
 
 ## Installation
 
-```
-git clone https://github.com/luizgabriel/conan-filament.git
-cd conan-filament
-conan export . google/stable
-```
-
 In your conanfile.txt, add:
 ```
 [requires]
-filament/1.4.5@google/stable
+filament/1.5.2@google/stable
 
 [imports]
 # imports filament tools
@@ -23,44 +17,41 @@ bin, * -> ./bin
 To build Filament, you must first install the following tools:
 
 - CMake 3.10 (or more recent)
-- clang 7.0 (or more recent)
+- clang 9.0 (or more recent)
 - ninja 1.8 (or more recent)
 
 ### Necessary Steps (Linux Only)
 Make sure you've installed the following dependencies:
 
-- `clang-7` or higher
+- `clang` or higher
 - `libglu1-mesa-dev`
-- `libc++-7-dev` (libcxx-devel and libcxx-static on Fedora) or higher
-- `libc++abi-7-dev` (libcxxabi-static on Fedora) or higher
+- `libc++-dev` (libcxx-devel and libcxx-static on Fedora) or higher
+- `libc++abi-dev` (libcxxabi-static on Fedora) or higher
 - `ninja-build`
 - `libxi-dev`
 
 ### Necessary Steps (Windows Only)
-If you're using Windows, you'll need some extra steps to make it work. By default, Conan will look for a MinGW installation looking for GCC, and that's not what we want. You'll need to configure a special conan profile to be able with compile correctly this package. So, find the profiles folder in `C:\\Users\%USERNAME%\.conan\profiles` and create a file called `clang` (without extension) and put this:
+If you're using Windows, you'll need some extra steps to make it work. By default, Conan will look for a MinGW installation looking for GCC, and that's not what we want. You'll need to configure your conan profile to be able with compile correctly this package.
 ```
 [settings]
-os=Windows
-os_build=Windows
-arch=x86_64
-arch_build=x86_64
+filament:compiler=Visual Studio
+filament:compiler.version=16
+filament:compiler.toolset=ClangCl
+filament:compiler.runtime=MTd
 
-compiler=Visual Studio
-compiler.version=16
-compiler.toolset=LLVM
-compiler.runtime=MTd
-
-build_type=Debug
+[env]
+filament:CC=C://PROGRA~1/LLVM/bin/clang-cl.exe
+filament:CXX=C://PROGRA~1/LLVM/bin/clang-cl.exe
 ```
 
 Requirements:
-- Visual Studio 2019 and 2017 (Yes, both)
-- The [LLVM Toolchain](https://marketplace.visualstudio.com/items?itemName=LLVMExtensions.llvm-toolchain)
+- Visual Studio 2019
+- LLVM
 
-In other to this `LLVM` toolchain to work, you'll need to change the `.conan\settings.yml` file with:
+In other to this `ClangCl` toolchain to work, you'll need to change the `.conan\settings.yml` file with:
 ```yml
 Visual Studio: &visual_studio
-    runtime: [MD, MT, MTd, MDd] # <--- Add "MT" and "MTd"
+    runtime: [MD, MT, MTd, MDd]
     version: ["8", "9", "10", "11", "12", "14", "15", "16"]
     toolset: [None, v90, v100, v110, v110_xp, v120, v120_xp,
               v140, v140_xp, v140_clang_c2, LLVM-vs2012, LLVM-vs2012_xp,
--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,25 @@
+from conans import ConanFile, CMake, tools
 import os
 
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration, ConanException
+FILAMENT_FLAGS = {
+    "FILAMENT_ENABLE_JAVA": False,
+    "FILAMENT_SKIP_SAMPLES": True,
+    "FILAMENT_ENABLE_LTO": False,
+    "FILAMENT_BUILD_FILAMAT": False,
+    "FILAMENT_SUPPORTS_METAL": False,
+    "FILAMENT_SUPPORTS_VULKAN": False,
+    "FILAMENT_GENERATE_JS_DOCS": False,
+    "FILAMENT_INSTALL_BACKEND_TEST": False,
+    "FILAMENT_USE_EXTERNAL_GLES3": False,
+}
 
+
+def flag_to_option(flag):
+    return flag.replace("FILAMENT_", "").lower()
 
 class FilamentConan(ConanFile):
     name = "filament"
-    version = "1.4.5"
+    version = "1.5.2"
     license = "Apache License 2.0"
     homepage = "https://github.com/google/filament"
     url = "https://github.com/luizgabriel/conan-filament"
@@ -14,16 +27,10 @@ class FilamentConan(ConanFile):
                   "macOS and WASM/WebGL "
     topics = ("graphics", "3d", "filament", "google")
     settings = ("os", "compiler", "build_type", "arch")
-    options = {
-        "shared": [True, False],
-        "enable_java": [True, False]
-    }
-    default_options = {
-        "shared": False,
-        "enable_java": False
-    }
     generators = "cmake"
-    #build_requires = "cmake_installer/3.15.5@conan/stable"
+    build_requires = "cmake_installer/3.15.5@conan/stable"
+    options = {flag_to_option(opt): [True, False] for opt in FILAMENT_FLAGS.keys()}
+    default_options = {flag_to_option(opt): value for opt, value in FILAMENT_FLAGS.items()}
 
     def source(self):
         git = tools.Git(folder="filament")
@@ -33,17 +40,30 @@ class FilamentConan(ConanFile):
                               '''project(TNT)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
-        
+
     @property
     def opt_toolset(self):
         return self.settings.get_safe("compiler.toolset")
 
+    def _cmake_define(self, cmake, definition):
+        option = definition.replace("FILAMENT_", "").lower()
+        cmake.definitions[definition] = "ON" if self.options[option] else "OFF"
+
     def _configure_cmake(self):
         cmake = CMake(self, toolset=self.opt_toolset)
-        cmake.definitions["ENABLE_JAVA"] = "ON" if self.options["enable_java"] else "OFF"
-        cmake.definitions["BUILD_TESTING"] = "OFF"
         cmake.definitions["CONAN_DISABLE_CHECK_COMPILER"] = "ON"
-            
+
+        for flag in FILAMENT_FLAGS.keys():
+            cmake.definitions[flag] = "ON" if self.options[flag_to_option(flag)] else "OFF"
+
+        cc = self.env.get("CC", None)
+        if cc and os.path.exists(cc):
+            cmake.definitions["CMAKE_C_COMPILER"] = cc
+
+        cxx = self.env.get("CXX", None)
+        if cxx and os.path.exists(cxx):
+            cmake.definitions["CMAKE_CXX_COMPILER"] = cxx
+
         cmake.configure(source_dir="filament")
 
         return cmake
