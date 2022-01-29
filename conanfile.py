4a0f893b1ee3f7c998b6aceb7deb4daf7031bc57
Luiz Gabriel <luizgabriel.info@gmail.com>
2022-01-29 18:16:15 -0300

    Update to version 1.17.0

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class FilamentConan(ConanFile):
     name = "filament"
-    version = "1.9.21"
+    version = "1.17.0"
     license = "Apache License 2.0"
     homepage = "https://github.com/google/filament"
     url = "https://github.com/luizgabriel/conan-filament"
@@ -12,12 +12,14 @@ class FilamentConan(ConanFile):
     topics = ("graphics", "3d", "filament", "google")
     settings = ("os", "compiler", "build_type", "arch")
     generators = "cmake"
-    build_requires = "cmake/3.16.3"
+    build_requires = "cmake/3.19.8"
     options = {
         "enable_java": [True, False],
         "skip_samples": [True, False],
         "enable_lto": [True, False],
         "build_filamat": [True, False],
+        "use_swiftshader": [True, False],
+        "supports_opengl": [True, False],
         "supports_metal": [True, False],
         "supports_vulkan": [True, False],
         "generate_js_docs": [True, False],
@@ -27,6 +29,8 @@ class FilamentConan(ConanFile):
         "skip_samples": True,
         "enable_lto": False,
         "build_filamat": True,
+        "use_swiftshader": False,
+        "supports_opengl": True,
         "supports_metal": False,
         "supports_vulkan": False,
         "generate_js_docs": False,
@@ -34,7 +38,7 @@ class FilamentConan(ConanFile):
 
     def source(self):
         git = tools.Git(folder="filament")
-        git.clone("https://github.com/google/filament.git", "v" + self.version)
+        git.clone("https://github.com/google/filament.git", "v" + self.version, shallow=True)
 
         tools.replace_in_file("filament/CMakeLists.txt", "project(TNT)",
                               '''project(TNT)
@@ -49,6 +53,8 @@ conan_basic_setup()
         cmake.definitions["FILAMENT_SKIP_SAMPLES"] = self.options.get_safe("skip_samples", False)
         cmake.definitions["FILAMENT_ENABLE_LTO"] = self.options.get_safe("enable_lto", False)
         cmake.definitions["FILAMENT_BUILD_FILAMAT"] = self.options.get_safe("build_filamat", False)
+        cmake.definitions["FILAMENT_USE_SWIFTSHADER"] = self.options.get_safe("use_swiftshader", False)
+        cmake.definitions["FILAMENT_SUPPORTS_OPENGL"] = self.options.get_safe("supports_opengl", False)
         cmake.definitions["FILAMENT_SUPPORTS_METAL"] = self.options.get_safe("supports_metal", False)
         cmake.definitions["FILAMENT_SUPPORTS_VULKAN"] = self.options.get_safe("supports_vulkan", False)
         cmake.definitions["FILAMENT_GENERATE_JS_DOCS"] = self.options.get_safe("generate_js_docs", False)
