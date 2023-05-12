26937691416dc35a84e1a772128a4c1f42552c0e
Luiz Gabriel <luizgabriel.info@gmail.com>
2023-05-12 01:24:58 -0300

    Update conanfile.py to Conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,86 +1,92 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
 
 
 class FilamentConan(ConanFile):
     name = "filament"
-    version = "1.18.0"
+    version = "1.35.0"
     license = "Apache License 2.0"
     homepage = "https://github.com/google/filament"
     url = "https://github.com/luizgabriel/conan-filament"
     description = "Filament is a real-time physically based rendering engine for Android, iOS, Windows, Linux, " \
                   "macOS and WASM/WebGL "
     topics = ("graphics", "3d", "filament", "google")
-    settings = ("os", "compiler", "build_type", "arch")
-    revision_mode = "hash"
-    generators = "cmake"
-    build_requires = [
-        "cmake/3.19.8",
-        "ninja/1.10.0"
+    settings = "os", "compiler", "build_type", "arch"
+
+    cmake_variables = [
+        "FILAMENT_USE_EXTERNAL_GLES3",  # Experimental: Compile Filament against OpenGL ES 3
+        "FILAMENT_USE_SWIFTSHADER",  # Compile Filament against SwiftShader
+        "FILAMENT_ENABLE_LTO",  # Enable link-time optimizations if supported by the compiler
+        "FILAMENT_SKIP_SAMPLES",  # Don't build samples
+        "FILAMENT_SUPPORTS_XCB",  # Include XCB support in Linux builds
+        "FILAMENT_SUPPORTS_XLIB",  # Include XLIB support in Linux builds
+        "FILAMENT_SUPPORTS_EGL_ON_LINUX",  # Use EGL for OpenGL in Linux builds
+        "FILAMENT_SUPPORTS_WAYLAND",  # Include Wayland support in Linux builds
+        "FILAMENT_SKIP_SDL2",  # Skip dependencies of SDL2, and SDL2
+        "FILAMENT_LINUX_IS_MOBILE",  # Treat Linux as Mobile
+        "FILAMENT_ENABLE_ASAN_UBSAN",  # Enable Address and Undefined Behavior Sanitizers
+        "FILAMENT_SUPPORTS_OPENGL",  # By default, build with support for OpenGL on all platforms.
+        "FILAMENT_SUPPORTS_METAL",  # Build with Metal support on non-WebGL Apple platforms.
+        "FILAMENT_SUPPORTS_VULKAN",
+        # build with Vulkan support on desktop platforms, although clients must request to use it at run time.
+        "FILAMENT_BUILD_FILAMAT",
+        # Building filamat increases build times and isn't required for web, so turn it off by default.
+        "FILAMENT_DISABLE_MATOPT",
+        # Only optimize materials in Release mode (so error message lines match the source code)
     ]
 
     options = {
-        "enable_java": [True, False],
-        "skip_samples": [True, False],
-        "enable_lto": [True, False],
-        "build_filamat": [True, False],
-        "use_swiftshader": [True, False],
-        "supports_opengl": [True, False],
-        "supports_metal": [True, False],
-        "supports_vulkan": [True, False],
-        "generate_js_docs": [True, False],
+        "use_external_gles3": [True, False, None],
+        "use_swiftshader": [True, False, None],
+        "enable_lto": [True, False, None],
+        "skip_samples": [True, False, None],
+        "supports_xcb": [True, False, None],
+        "supports_xlib": [True, False, None],
+        "supports_egl_on_linux": [True, False, None],
+        "supports_wayland": [True, False, None],
+        "skip_sdl2": [True, False, None],
+        "linux_is_mobile": [True, False, None],
+        "enable_asan_ubsan": [True, False, None],
+        "supports_opengl": [True, False, None],
+        "supports_metal": [True, False, None],
+        "supports_vulkan": [True, False, None],
+        "build_filamat": [True, False, None],
+        "disable_matopt": [True, False, None],
     }
+
     default_options = {
-        "enable_java": False,
         "skip_samples": True,
-        "enable_lto": False,
-        "build_filamat": True,
-        "use_swiftshader": False,
-        "supports_opengl": True,
-        "supports_metal": False,
-        "supports_vulkan": False,
-        "generate_js_docs": False,
+        "skip_sdl2": True,
     }
 
     def source(self):
-        git = tools.Git(folder="filament")
-        git.clone("https://github.com/google/filament.git", "v" + self.version, shallow=True)
+        git = Git(self)
+        git.clone(url="https://github.com/google/filament.git", target=".")
+        git.checkout("v" + self.version)
 
-        tools.replace_in_file("filament/CMakeLists.txt", "project(TNT)",
-                              '''project(TNT)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()
-''')
+    def layout(self):
+        cmake_layout(self)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["FILAMENT_ENABLE_JAVA"] = self.options.get_safe("enable_java", False)
-        cmake.definitions["FILAMENT_SKIP_SAMPLES"] = self.options.get_safe("skip_samples", False)
-        cmake.definitions["FILAMENT_ENABLE_LTO"] = self.options.get_safe("enable_lto", False)
-        cmake.definitions["FILAMENT_BUILD_FILAMAT"] = self.options.get_safe("build_filamat", False)
-        cmake.definitions["FILAMENT_USE_SWIFTSHADER"] = self.options.get_safe("use_swiftshader", False)
-        cmake.definitions["FILAMENT_SUPPORTS_OPENGL"] = self.options.get_safe("supports_opengl", False)
-        cmake.definitions["FILAMENT_SUPPORTS_METAL"] = self.options.get_safe("supports_metal", False)
-        cmake.definitions["FILAMENT_SUPPORTS_VULKAN"] = self.options.get_safe("supports_vulkan", False)
-        cmake.definitions["FILAMENT_GENERATE_JS_DOCS"] = self.options.get_safe("generate_js_docs", False)
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        for variable in self.cmake_variables:
+            value = self.options.get_safe(variable.replace("FILAMENT_", "").lower(), None)
+            if value is not None:
+                tc.variables[variable] = "ON" if value else "OFF"
 
-        cmake.configure(source_dir="filament")
-
-        return cmake
+        tc.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
-    def package_info(self):
-        self.cpp_info.libdirs = ["lib/x86_64", "lib/arm64"]
-        self.cpp_info.libs = [
-            "backend", "filaflat", "geometry", "matdbg",
-            "bluegl", "filamat", "gltfio", "meshoptimizer",
-            "camutils", "filamat_lite", "gltfio_core", "shaders",
-            "dracodec", "filament", "ibl", "smol-v",
-            "filabridge", "filameshio", "image", "utils"
-        ]
+    def build_requirements(self):
+        self.tool_requires("cmake/3.22.6")
